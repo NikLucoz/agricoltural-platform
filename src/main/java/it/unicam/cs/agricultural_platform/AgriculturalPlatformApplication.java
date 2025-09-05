e46343a0a5873b908e1ea333fe889809d8d7e4f2
NikLucoz <lucozzilavoro@gmail.com>
2025-09-05 15:22:30 +0200

    Init Repository

---
+++ src/main/java/it/unicam/cs/agricultural_platform/AgriculturalPlatformApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.agricultural_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturalPlatformApplication.class, args);
+	}
+
+}
