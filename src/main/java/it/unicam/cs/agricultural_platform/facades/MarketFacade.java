7c588d4ce9e68b0b6e6e507c2672f313ae0820f4
NikLucoz <lucozzilavoro@gmail.com>
2025-11-22 13:07:43 +0100

    [Fix] Market Middlewares

--- src/main/java/it/unicam/cs/agricultural_platform/facades/MarketFacade.java
+++ src/main/java/it/unicam/cs/agricultural_platform/facades/MarketFacade.java
@@ -1,10 +1,18 @@
 package it.unicam.cs.agricultural_platform.facades;
 
+import it.unicam.cs.agricultural_platform.dto.content.UpdateMarketDataDTO;
+import it.unicam.cs.agricultural_platform.dto.user.ItemCartOperationDTO;
+import it.unicam.cs.agricultural_platform.middlewares.Middleware;
+import it.unicam.cs.agricultural_platform.middlewares.market.ItemCartOperationMiddleware;
+import it.unicam.cs.agricultural_platform.middlewares.market.UpdateMarketDataMiddleware;
+import it.unicam.cs.agricultural_platform.middlewares.user.UserDataMiddleware;
+import it.unicam.cs.agricultural_platform.middlewares.user.UserPasswordMiddleware;
 import it.unicam.cs.agricultural_platform.models.user.User;
 import it.unicam.cs.agricultural_platform.services.MarketService;
 import it.unicam.cs.agricultural_platform.services.ProductPacketService;
 import it.unicam.cs.agricultural_platform.services.ProductService;
 import it.unicam.cs.agricultural_platform.services.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,19 +25,36 @@ public class MarketFacade {
     @Autowired
     private UserService userService;
 
-    public boolean addItemToUserCart(long userId, long id, int quantity){
-        if(!userService.existsUser(userId)) return false;
-        var user = userService.getUserById(userId);
-        return marketService.addItemToUserCart(user, id, quantity);
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductPacketService productPacketService;
+
+
+    private Middleware<ItemCartOperationDTO> itemCartOperationMiddleware;
+    private Middleware<UpdateMarketDataDTO> updateMarketDataMiddleware;
+
+    @PostConstruct
+    private void init(){
+        itemCartOperationMiddleware = Middleware.link(new ItemCartOperationMiddleware(userService, productService, productPacketService));
+        updateMarketDataMiddleware = Middleware.link(new UpdateMarketDataMiddleware(productService, productPacketService));
     }
 
-    public boolean removeItemFromUserCart(long userId, long id, int quantity){
-        if(!userService.existsUser(userId)) return false;
-        var user = userService.getUserById(userId);
-        return marketService.removeItemFromUserCart(user, id, quantity);
+    public boolean addItemToUserCart(ItemCartOperationDTO itemCartOperationDTO){
+        if(!itemCartOperationMiddleware.handle(itemCartOperationDTO)) return false;
+        var user = userService.getUserById(itemCartOperationDTO.getUserId());
+        return marketService.addItemToUserCart(user, itemCartOperationDTO.getContentId(), itemCartOperationDTO.getQuantity());
     }
 
-    public boolean updateContentMarketData(long id, double price, int stockQuantity){
-        return marketService.updateContentMarketData(id, price, stockQuantity);
+    public boolean removeItemFromUserCart(ItemCartOperationDTO itemCartOperationDTO){
+        if(!itemCartOperationMiddleware.handle(itemCartOperationDTO)) return false;
+        var user = userService.getUserById(itemCartOperationDTO.getUserId());
+        return marketService.removeItemFromUserCart(user, itemCartOperationDTO.getContentId(), itemCartOperationDTO.getQuantity());
+    }
+
+    public boolean updateContentMarketData(UpdateMarketDataDTO updateDTO){
+        if(!updateMarketDataMiddleware.handle(updateDTO)) return false;
+        return marketService.updateContentMarketData(updateDTO.getContentId(), updateDTO.getPrice(), updateDTO.getStockQuantity());
     }
 }
---
+++ src/main/java/it/unicam/cs/agricultural_platform/middlewares/market/ItemCartOperationMiddleware.java
@@ -0,0 +1,30 @@
+package it.unicam.cs.agricultural_platform.middlewares.market;
+
+import it.unicam.cs.agricultural_platform.dto.user.ItemCartOperationDTO;
+import it.unicam.cs.agricultural_platform.middlewares.Middleware;
+import it.unicam.cs.agricultural_platform.services.ProductPacketService;
+import it.unicam.cs.agricultural_platform.services.ProductService;
+import it.unicam.cs.agricultural_platform.services.UserService;
+
+public class ItemCartOperationMiddleware extends Middleware<ItemCartOperationDTO> {
+
+    private final UserService userService;
+    private final ProductService productService;
+    private final ProductPacketService productPacketService;
+
+    public ItemCartOperationMiddleware(UserService userService, ProductService productService, ProductPacketService productPacketService) {
+        this.userService = userService;
+        this.productService = productService;
+        this.productPacketService = productPacketService;
+    }
+
+    @Override
+    public boolean handle(ItemCartOperationDTO data) {
+        if(!userService.existsUser(data.getUserId())) return false;
+        if(!productService.existsProduct(data.getContentId())) {
+            if(!productPacketService.existsPacket(data.getContentId())) return false;
+        }
+        if(data.getQuantity() < 0) return false;
+        return handleNext(data);
+    }
+}
